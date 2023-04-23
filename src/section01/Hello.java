8ad27304f3a7f347a071584f2c21c10374423c8d
Your Name <you@example.com>
2023-04-23 14:37:57 +0900

    First commit

---
+++ src/section01/Hello.java
@@ -0,0 +1,28 @@
+package section01;
+
+public class Hello {
+	/* 여러줄 주석
+	 * 
+	 * 
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello,JAVA!!!");
+	
+		printNum(422);
+	}
+	
+	/** JavaDoc 주석
+	 * auth : 김혜성
+	 * 생성일 : 2023.04.22
+	 * 
+	 * printNum - 입력받은 정수 출력
+	 * 
+	 * @param num
+	 */
+	public static void printNum(int num) {
+		System.out.println("num:" + num);
+	}
+	
+	
+}
+
---
+++ src/section02/ConsolePrint.java
@@ -0,0 +1,26 @@
+package section02;
+/*
+ * 출력문
+ *  테이터를 화면에 출력하는 방법
+ * 
+ * System.out.println() : 괄호안 내용출력 후 행 바꿈
+ * System.out.print() : 괄호안 내용출력 행 바꿈x
+ * System.out.printf() : 문자열을 서식 문자를 이용해 형식화된 내용 출력 행 바꿈X
+ * 
+ * 
+ */
+
+public class ConsolePrint {
+
+	public static void main(String[] args) {
+		System.out.print("Welcom.");
+		
+		System.out.println("Java Welcom.");
+		
+		System.out.printf("오늘은 %d월 %d일 입니다.", 4,22);
+
+
+	}
+
+
+}
